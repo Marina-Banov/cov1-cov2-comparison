c1dc5bf777a3afb460332f2357b5fad32403c5c2
mbanov <m.banov7@gmail.com>
2021-01-25 16:49:29 +0100

    subplots

--- gui.py
+++ gui.py
@@ -3,8 +3,6 @@ from PyQt5.QtWidgets import QComboBox, QLabel
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt
 import data_init as dt
-import time
-import numpy as np
 from matplotlib.backends.qt_compat import QtCore, QtWidgets
 import mplcursors
 from matplotlib.ticker import MaxNLocator
@@ -18,14 +16,6 @@ else:
         FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
     from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
-
-
-class MplCanvas(FigureCanvasQTAgg):
-
-    def __init__(self):
-        fig = Figure(figsize=(4, 4), dpi=100)
-        self.axes = fig.add_subplot(111)
-        super(MplCanvas, self).__init__(fig)
 
 
 class App(QtWidgets.QMainWindow):
@@ -47,11 +37,6 @@ class App(QtWidgets.QMainWindow):
         self.setWindowIcon(QIcon('img/covid'))
         self._main = QtWidgets.QWidget()
         self.setCentralWidget(self._main)
-
-        # self.covGraph  = QLabel(self)
-        # imagePath = '4jsm10.jpg'
-        # pixmap = QPixmap(imagePath)
-        # self.covGraph.setPixmap(pixmap.scaled(640 , 640, Qt.KeepAspectRatio))
 
         self.dropdown = QComboBox()
         self.dropdown.setObjectName('dropdownList')
@@ -75,56 +60,52 @@ class App(QtWidgets.QMainWindow):
         self.grid.addWidget(self.cov2Txt, 2, 4, 1, 2)
         self.grid.addWidget(self.cov1FirstCase, 3, 0, 1, 2)
         self.grid.addWidget(self.cov2FirstCase, 3, 4, 1, 2)
-        # grid.addWidget(self.covGraph, 4, 0, 4, 4)
 
-        self.sc = MplCanvas()
-        self.grid.addWidget(self.sc, 4, 0, 4, 4)
-        self.update_graph('Australia')
-        self.mpl = self.set_mpl()
-
-        # dynamic_canvas = FigureCanvas(Figure(figsize=(4,4)))
-        # grid.addWidget(dynamic_canvas, 4, 4, 4, 4)
-        # self._dynamic_ax = dynamic_canvas.figure.subplots()
-        # # Set up a Line2D.
-        # self._line, = self._dynamic_ax.plot(cov1.get_group('China')['date'], cov1.get_group('China')['new_confirmed'])
-        # self._timer = dynamic_canvas.new_timer(50)
-        # self._timer.add_callback(self._update_canvas)
-        # self._timer.start()
+        self.sc = FigureCanvasQTAgg(Figure(figsize=(4, 4)))
+        self.sc.figure.add_subplot(2, 2, 1)
+        self.sc.figure.add_subplot(2, 2, 2)
+        self.sc.figure.add_subplot(2, 2, 3)
+        self.sc.figure.add_subplot(2, 2, 4)
+        self.grid.addWidget(self.sc, 4, 0, 8, 8)
+        self.update_all_graphs('Australia')
+        self.set_mpl()
 
         self.grid.setAlignment(Qt.AlignCenter)
         self.setLayout(self.grid)
 
-    def update_graph(self, country):
+    def update_all_graphs(self, country):
         self.sc.figure.set_canvas(self.sc)
-        self.sc.axes.clear()
-        cov1.get_group(country).plot(ax=self.sc.axes,
-                                     x='date',
-                                     y=['total_confirmed', 'total_deceased', 'total_recovered'],
-                                     color=['r', 'k', 'g'],
-                                     xlabel='')
-        self.sc.axes.yaxis.set_major_locator(MaxNLocator(integer=True))
+        all_axes = self.sc.figure.get_axes()
+        totals = ['total_confirmed', 'total_deceased', 'total_recovered']
+        new = ['new_confirmed', 'new_deceased', 'new_recovered']
+        g1 = cov1.get_group(country)
+        g2 = cov2.get_group(country)
+        update_graph(g1, totals, all_axes[0])
+        update_graph(g2, totals, all_axes[1])
+        update_graph(g1, new, all_axes[2])
+        update_graph(g2, new, all_axes[3])
         self.sc.figure.canvas.draw()
 
     def set_mpl(self):
-        mpl = mplcursors.cursor(self.sc.axes,
-                                hover=2,
-                                highlight=True,
-                                highlight_kwargs={'color': 'b', 'lw': 3})
-        mpl.connect('add', lambda sel: sel.annotation.get_bbox_patch().set(fc='white', alpha=1))
-        mpl.connect('add', lambda sel: sel.annotation.set_text(
-            'x = {}\ny = {}'.format(sel.target[0], int(sel.target[1]))))
-        return mpl
+        all_axes = self.sc.figure.get_axes()
+        for i in range(4):
+            mpl = mplcursors.cursor(all_axes[i], hover=2,
+                                    highlight=True, highlight_kwargs={'color': 'b', 'lw': 3})
+            mpl.connect('add', lambda sel: sel.annotation.get_bbox_patch().set(fc='white', alpha=1))
+            mpl.connect('add', lambda sel: sel.annotation.set_text(
+                '{} ({})'.format(int(sel.target[0]), int(sel.target[1]))))
 
     def on_combobox_changed(self, value):
         print("combobox changed", value)
-        self.update_graph(value)
-        self.mpl = self.set_mpl()
+        self.update_all_graphs(value)
+        self.set_mpl()
 
-    def _update_canvas(self):
-        t = np.linspace(0, 10, 101)
-        # Shift the sinusoid as a function of time.
-        self._line.set_data(t, np.sin(t + time.time()))
-        self._line.figure.canvas.draw()
+
+def update_graph(group, data, ax):
+    ax.clear()
+    group.plot(ax=ax, x='date', y=data,
+               color=['r', 'k', 'g'], xlabel='')
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
 
 if __name__ == '__main__':
