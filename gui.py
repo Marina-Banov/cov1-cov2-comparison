a7abda4d0b431e1fefff066d4dbf16f62fc67884
mbanov <m.banov7@gmail.com>
2021-01-28 19:07:32 +0100

    export data

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QComboBox
+import os
+from PyQt5.QtWidgets import QComboBox, QPushButton
 from PyQt5.QtGui import QIcon, QFont
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -11,6 +12,7 @@ else:
 from matplotlib.figure import Figure
 from matplotlib.ticker import MaxNLocator
 import mplcursors
+import pandas as pd
 import data_init as dt
 
 
@@ -29,11 +31,14 @@ class App(QtWidgets.QMainWindow):
         self.setCentralWidget(self._main)
 
         dropdown = QComboBox()
-        dropdown.setObjectName('dropdownList')
         dropdown.addItems(cov2.groups.keys())
         dropdown.resize(100, 50)
         dropdown.setFont(QFont('Ubuntu', 11, QFont.Medium))
         dropdown.currentTextChanged.connect(self.on_combobox_changed)
+
+        button = QPushButton('Snimi podatke')
+        button.setFont(QFont('Ubuntu', 11, QFont.Medium))
+        button.clicked.connect(self.on_button_click)
 
         self.canvas = FigureCanvasQTAgg(Figure(figsize=(4, 4)))
         self.canvas.figure.add_subplot(2, 2, 1)
@@ -41,33 +46,35 @@ class App(QtWidgets.QMainWindow):
         self.canvas.figure.add_subplot(2, 2, 3)
         self.canvas.figure.add_subplot(2, 2, 4)
 
-        self.update_all_graphs(list(cov2.groups.keys())[0])
+        self.country = list(cov2.groups.keys())[0]
+        self.update_all_graphs()
         set_mpl(self.canvas.figure.get_axes())
 
         grid = QtWidgets.QGridLayout(self._main)
         grid.setSpacing(20)
         grid.addWidget(dropdown, 1, 3, 1, 2)  # row, col, rowspan, colspan
+        grid.addWidget(button, 1, 5, 1, 1)
         grid.addWidget(self.canvas, 3, 0, 8, 8)
         grid.setContentsMargins(50, 50, 50, 50)
         self.setLayout(grid)
 
         self.showMaximized()
 
-    def update_all_graphs(self, country):
+    def update_all_graphs(self):
         self.canvas.figure.set_canvas(self.canvas)
         all_axes = self.canvas.figure.get_axes()
         totals = ['total_confirmed', 'total_deceased', 'total_recovered']
         new = ['new_confirmed', 'new_deceased', 'new_recovered']
 
         try:
-            g1 = cov1.get_group(country)
+            g1 = cov1.get_group(self.country)
             update_graph(g1, totals, all_axes[0], 'SARS', True)
             update_graph(g1, new, all_axes[2], '', True)
         except KeyError as e:
             update_graph(None, None, all_axes[0], 'SARS', False)
             update_graph(None, None, all_axes[2], '', False)
 
-        g2 = cov2.get_group(country)
+        g2 = cov2.get_group(self.country)
         update_graph(g2, totals, all_axes[1], 'COVID-19', True)
         update_graph(g2, new, all_axes[3], '', True)
 
@@ -75,8 +82,17 @@ class App(QtWidgets.QMainWindow):
         self.canvas.figure.canvas.draw()
 
     def on_combobox_changed(self, value):
-        self.update_all_graphs(value)
+        self.country = value
+        self.update_all_graphs()
         set_mpl(self.canvas.figure.get_axes())
+
+    def on_button_click(self):
+        g2 = cov2.get_group(self.country)
+        try:
+            g1 = cov1.get_group(self.country)
+            pd.concat([g1, g2]).to_csv(os.getcwd()+'\\out\\{}.csv'.format(self.country), index=False, header=True)
+        except KeyError as e:
+            g2.to_csv(os.getcwd()+'\\out\\{}.csv'.format(self.country), index=False, header=True)
 
 
 def set_mpl(all_axes):
